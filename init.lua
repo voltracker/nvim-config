531a68b257fda477b9128c8319892d9dfd37db3a
Matthew Davidson <matthewd0224@gmail.com>
2022-12-13 01:02:43 +0100

    initial commit

---
+++ init.lua
@@ -0,0 +1,25 @@
+vim.opt.number = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.culopt = "both"
+vim.opt.list = true
+
+require('plugins')
+require('lualine').setup{
+  options = {
+    theme = 'gruvbox'
+  }
+}
+--require('lspconfig').setup{}
+require('nvim-treesitter.configs').setup {
+  ensure_installed = "all",
+  sync_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  }
+}
