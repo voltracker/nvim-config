dbf0525b3f9c86f75b023efd79974adfb352bf35
Matthew Davidson <matthewd0224@gmail.com>
2022-12-22 19:53:45 +0000

    added telescope

--- lua/voltracker/plugins.lua
+++ lua/voltracker/plugins.lua
@@ -32,4 +32,9 @@ return require('packer').startup(function(use)
       {'rafamadriz/friendly-snippets'},
     }
   }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
